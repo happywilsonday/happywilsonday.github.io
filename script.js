f05aef98d729701047da0a75ae3a62bce791272c
YiQingK <khooyiqing@gmail.com>
2024-08-25 20:47:08 +0800

    tried diff carousel, fit artist name and indicator to viewbox

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-$('.carousel').carousel({
-    interval:0
-})
+$(".carousel").carousel({
+	interval: 0,
+});
 
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
+$(document).on("click", '[data-toggle="lightbox"]', function (event) {
+	event.preventDefault();
+	$(this).ekkoLightbox();
+});
+
+$(document).ready(function () {
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 10,
+		nav: true,
+		center: true,
+	});
 });
