eb9b4264f8d80445d53ffa3caea3fd63286d8df1
ivyleaf33 <ivyleaf33.dollars@gmail.com>
2024-09-01 23:46:37 -0400

    copying owl js

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-$('.carousel').carousel({
-    interval:0
-})
+// $('.carousel').carousel({
+//     interval:0
+// })
 
 $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
+});
+
+$(document).ready(function () {
+	var owl = $(".owl-carousel");
+
+	owl.owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 10,
+		nav: true,
+		center: true,
+	});
+
+	/*keyboard navigation*/
+	$(document.documentElement).keyup(function (event) {
+		if (event.keyCode == 37) {
+			/*left key*/
+			owl.trigger("prev.owl.carousel", [700]);
+		} else if (event.keyCode == 39) {
+			/*right key*/
+			owl.trigger("next.owl.carousel", [700]);
+		}
+	});
 });
